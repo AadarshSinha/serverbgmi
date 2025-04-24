bb98a8c02debf2e47150470a24cbd6f7cdc96dd4
Aadarsh Sinha <aadarshraj56@gmail.com>
2025-04-25 00:28:34 +0530

    refactor: update image file handling and prediction URL in test.py

--- app.py
+++ app.py
@@ -8,66 +8,114 @@ from ultralytics import YOLO
 import traceback
 import json
 from pathlib import Path
-
+import os
 
 def getPath():
     script_dir = str(Path(__file__).parent)
     prefix = script_dir.split("serverbgmi")[0] + "serverbgmi"
     return prefix
 path_prefix = getPath()
-print(f"Path prefix : {path_prefix}")
+
 app = Flask(__name__)
+
 sift = cv2.SIFT_create()
 index_params = dict(algorithm=1, trees=20)
 search_params = dict(checks=50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
+
 combine_map_normal = cv2.imread(f'{path_prefix}/Map/combine_new.png',  cv2.IMREAD_COLOR)
 combine_map = cv2.cvtColor(combine_map_normal, cv2.COLOR_BGR2GRAY)
 combine_map_kp, combine_map_des = sift.detectAndCompute(combine_map, None)
+
+model_circle_detection = YOLO(f'{path_prefix}/Models/bestFull.pt')
+
 with open(f"{path_prefix}/constants.json", "r") as file:
     map_constants = json.load(file)
-model_circle_detection = YOLO(f'{path_prefix}/Models/bestFull.pt')
-model_path = f"{path_prefix}/Models/e12/model.h5"
-scaler_path = f"{path_prefix}/Models/e12/scaler.pkl"
-model_e12 = keras.models.load_model(model_path, compile=False)
 
-with open(scaler_path, 'rb') as f:
-    scalers = pickle.load(f)
-    scaler_X = scalers['scaler_X']
-    scaler_y = scalers['scaler_y']
+models_dict = {}
+
+def initialize_models():
+    global models_dict
+    models_dict.clear()
+    model_names = ["e12", "e23", "e34", "e45", "e56", "e67", "e78",
+                   "m12", "m23", "m34", "m45", "m56", "m67", "m78",
+                   "v12", "v23", "v34", "v45", "v56", "v67", "v78",
+                   "s12", "s23", "s34", "s45", "s56", "s67", "s78"]
+    for name in model_names:
+        model_path = f"{path_prefix}/Models/{name}/model.h5"
+        scaler_path = f"{path_prefix}/Models/{name}/scaler.pkl"
+
+        model = None
+        scaler = None
+
+        if os.path.exists(model_path):
+            model = keras.models.load_model(model_path, compile=False)
+        else:
+            print(f"⚠️ Warning: Model '{name}' not found at {model_path}")
+
+        if os.path.exists(scaler_path):
+            with open(scaler_path, "rb") as f:
+                scaler = pickle.load(f)
+        else:
+            print(f"⚠️ Warning: Scaler '{name}' not found at {scaler_path}")
+
+        if model and scaler:
+            models_dict[name] = {"model": model, "scaler": scaler}
+
+initialize_models()
+
+print("Server started . . . ")
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Server is live!"})
 
 @app.route("/predict", methods=["POST"])
-def predict():
-    if "file" not in request.files:
-        return jsonify({"error": "No file provided"}), 400
-    
+def predict():    
     print("Got request")
+
     file = request.files["file"]
     file_bytes = np.frombuffer(file.read(), np.uint8)
-    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)    
-    zone_data = get_template_cordinates(image)
-    print(f" ZONE DATA : {zone_data}")
-    input = np.array([[zone_data["template_center"][0], zone_data["template_center"][1]]])
-    input_data = scaler_X.transform(input).reshape((1, 1, 2))
-    output_scaled = model_e12.predict(input_data)
-    output = scaler_y.inverse_transform(output_scaled)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    print(f"OUTPUT : {output}")
-    target_x = (zone_data["frame_center"][0]*output[0][0])/zone_data["template_center"][0]
-    target_y = (zone_data["frame_center"][1]*output[0][1])/zone_data["template_center"][1]
-    target_radius = (zone_data["frame_radius"] * get_zone_value(zone_data["template_radius"], "Erangle"))/zone_data["template_radius"]
-    target_zone = {"center": (target_x, target_y), "radius": target_radius}
-    print(f"Predicted zone cordinated on frame {target_zone}")
+    zone_data = get_template_cordinates(image)
+    print(f"zone_data = {zone_data}")
+    map_type = getMapType(zone_data["template_center"])
+    zone_value, zone_number = get_target_zone_radius_and_current_zone(zone_data["template_radius"], map_type)
+    input = np.array([[zone_data["template_center"][0], zone_data["template_center"][1]]])
+    output = get_template_predicted_zone(map_type, zone_number, input)
+    target_x, target_y = get_frame_predicted_zone(output, zone_data["matrix"])
+    target_radius = (zone_data["frame_radius"] * zone_value)/zone_data["template_radius"]
+    target_zone = {"predicted_center": (float(target_x), float(target_y)), "predicted_radius": target_radius, "frame_center": zone_data["frame_center"] }
+
+    save_result(output[0][0], output[0][1], zone_value, "result1")
+    print(f"Predicted zone cordinated on frame {target_zone} MAP : {map_type} Current Zone : {zone_number}")
 
     return target_zone
 
+def get_frame_predicted_zone(output, matrix):
+    predicted_template_point = np.array([[output[0]]], dtype=np.float32)
+    predicted_image_point = cv2.perspectiveTransform(predicted_template_point, np.linalg.inv(matrix))  
+    target_x, target_y = predicted_image_point[0][0]
+    return target_x, target_y
+
+def get_template_predicted_zone(map_type, zone_number, input):
+    model_name = f'{map_type}{zone_number}{zone_number+1}'
+    input_data = models_dict[model_name]["scaler"]["scaler_X"].transform(input).reshape((1, 1, 2))
+    output_scaled = models_dict[model_name]["model"].predict(input_data)
+    output = models_dict[model_name]["scaler"]["scaler_y"].inverse_transform(output_scaled)
+    return output
+
+def save_result(center_x, center_y, radius, file):
+    result_path = f"/Users/aadarshsinha/Desktop/VsCode/serverbgmi/TestResult/{file}.jpg"
+    temp_map = combine_map_normal.copy()
+    cv2.circle(temp_map, (int(center_x), int(center_y)), int(radius), (255, 0, 0), 2)    
+    cv2.imwrite(result_path, temp_map)
+    print(f"Results saved x : {center_x} y : {center_y} r : {radius}")
+
 def get_template_cordinates(image):
     best_bbox = None
-    zone = {"frame_center": (0, 0), "frame_radius": -1, "template_center": (0, 0), "template_radius": -1}
+    zone = {"frame_center": (0, 0), "frame_radius": -1, "template_center": (0, 0), "template_radius": -1, "matrix": None}
     try:
         results = model_circle_detection.predict(source=image, save=False, imgsz=1024, verbose=False)
         best_confidence = 0.0
@@ -117,90 +165,103 @@ def get_template_cordinates(image):
                     abs(mapped_bbox_pts[0][0][0] - mapped_bbox_pts[1][0][0]),
                     abs(mapped_bbox_pts[0][0][1] - mapped_bbox_pts[2][0][1])
                 ) // 2)
-
                 zone["template_center"] = (center_x, center_y) 
                 zone["template_radius"] =  radius
+                zone["matrix"] =  matrix
     except:
         stack_trace = traceback.format_exc()
         print(stack_trace)
     
     return zone
 
-def get_zone_value(radius, map):
+def get_target_zone_radius_and_current_zone(radius, map):
     match map:
-        case "Erangle":
+        case "e":
             match radius:
                 case _ if map_constants["erangle"]["1"][0] <= radius <= map_constants["erangle"]["1"][1]:
-                    return map_constants["erangle"]["2"][0]
+                    return map_constants["erangle"]["2"][0], 1
                 case _ if map_constants["erangle"]["2"][0] <= radius <= map_constants["erangle"]["2"][1]:
-                    return map_constants["erangle"]["3"][0]
+                    return map_constants["erangle"]["3"][0], 2
                 case _ if map_constants["erangle"]["3"][0] <= radius <= map_constants["erangle"]["3"][1]:
-                    return map_constants["erangle"]["4"][0]
+                    return map_constants["erangle"]["4"][0], 3
                 case _ if map_constants["erangle"]["4"][0] <= radius <= map_constants["erangle"]["4"][1]:
-                    return map_constants["erangle"]["5"][0]
+                    return map_constants["erangle"]["5"][0], 4
                 case _ if map_constants["erangle"]["5"][0] <= radius <= map_constants["erangle"]["5"][1]:
-                    return map_constants["erangle"]["6"][0]
+                    return map_constants["erangle"]["6"][0], 5
                 case _ if map_constants["erangle"]["6"][0] <= radius <= map_constants["erangle"]["6"][1]:
-                    return map_constants["erangle"]["7"][0]
+                    return map_constants["erangle"]["7"][0], 6
                 case _ if map_constants["erangle"]["7"][0] <= radius <= map_constants["erangle"]["7"][1]:
-                    return map_constants["erangle"]["8"][0]
+                    return map_constants["erangle"]["8"][0], 7
                 case _ if map_constants["erangle"]["8"][0] <= radius <= map_constants["erangle"]["8"][1]:
-                    return 0
-        case "Shanok":
+                    return 0, 8
+        case "s":
             match radius:
                 case _ if map_constants["shanok"]["1"][0] <= radius <= map_constants["shanok"]["1"][1]:
-                    return map_constants["shanok"]["2"][0]
+                    return map_constants["shanok"]["2"][0], 1
                 case _ if map_constants["shanok"]["2"][0] <= radius <= map_constants["shanok"]["2"][1]:
-                    return map_constants["shanok"]["3"][0]
+                    return map_constants["shanok"]["3"][0], 2
                 case _ if map_constants["shanok"]["3"][0] <= radius <= map_constants["shanok"]["3"][1]:
-                    return map_constants["shanok"]["4"][0]
+                    return map_constants["shanok"]["4"][0], 3
                 case _ if map_constants["shanok"]["4"][0] <= radius <= map_constants["shanok"]["4"][1]:
-                    return map_constants["shanok"]["5"][0]
+                    return map_constants["shanok"]["5"][0], 4
                 case _ if map_constants["shanok"]["5"][0] <= radius <= map_constants["shanok"]["5"][1]:
-                    return map_constants["shanok"]["6"][0]
+                    return map_constants["shanok"]["6"][0], 5
                 case _ if map_constants["shanok"]["6"][0] <= radius <= map_constants["shanok"]["6"][1]:
-                    return map_constants["shanok"]["7"][0]
+                    return map_constants["shanok"]["7"][0], 6
                 case _ if map_constants["shanok"]["7"][0] <= radius <= map_constants["shanok"]["7"][1]:
-                    return map_constants["shanok"]["8"][0]
+                    return map_constants["shanok"]["8"][0], 7
                 case _ if map_constants["shanok"]["8"][0] <= radius <= map_constants["shanok"]["8"][1]:
-                    return 0
-        case "Miramar":
+                    return 0, 8
+        case "m":
             match radius:
                 case _ if map_constants["miramar"]["1"][0] <= radius <= map_constants["miramar"]["1"][1]:
-                    return map_constants["miramar"]["2"][0]
+                    return map_constants["miramar"]["2"][0], 1
                 case _ if map_constants["miramar"]["2"][0] <= radius <= map_constants["miramar"]["2"][1]:
-                    return map_constants["miramar"]["3"][0]
+                    return map_constants["miramar"]["3"][0], 2
                 case _ if map_constants["miramar"]["3"][0] <= radius <= map_constants["miramar"]["3"][1]:
-                    return map_constants["miramar"]["4"][0]
+                    return map_constants["miramar"]["4"][0], 3
                 case _ if map_constants["miramar"]["4"][0] <= radius <= map_constants["miramar"]["4"][1]:
-                    return map_constants["miramar"]["5"][0]
+                    return map_constants["miramar"]["5"][0], 4
                 case _ if map_constants["miramar"]["5"][0] <= radius <= map_constants["miramar"]["5"][1]:
-                    return map_constants["miramar"]["6"][0]
+                    return map_constants["miramar"]["6"][0], 5
                 case _ if map_constants["miramar"]["6"][0] <= radius <= map_constants["miramar"]["6"][1]:
-                    return map_constants["miramar"]["7"][0]
+                    return map_constants["miramar"]["7"][0], 6
                 case _ if map_constants["miramar"]["7"][0] <= radius <= map_constants["miramar"]["7"][1]:
-                    return map_constants["miramar"]["8"][0]
+                    return map_constants["miramar"]["8"][0], 7
                 case _ if map_constants["miramar"]["8"][0] <= radius <= map_constants["miramar"]["8"][1]:
-                    return 0
-        case "Vikendi":
+                    return 0, 8
+        case "v":
             match radius:
                 case _ if map_constants["vikendi"]["1"][0] <= radius <= map_constants["vikendi"]["1"][1]:
-                    return map_constants["vikendi"]["2"][0]
+                    return map_constants["vikendi"]["2"][0], 1
                 case _ if map_constants["vikendi"]["2"][0] <= radius <= map_constants["vikendi"]["2"][1]:
-                    return map_constants["vikendi"]["3"][0]
+                    return map_constants["vikendi"]["3"][0], 2
                 case _ if map_constants["vikendi"]["3"][0] <= radius <= map_constants["vikendi"]["3"][1]:
-                    return map_constants["vikendi"]["4"][0]
+                    return map_constants["vikendi"]["4"][0], 3
                 case _ if map_constants["vikendi"]["4"][0] <= radius <= map_constants["vikendi"]["4"][1]:
-                    return map_constants["vikendi"]["5"][0]
+                    return map_constants["vikendi"]["5"][0], 4
                 case _ if map_constants["vikendi"]["5"][0] <= radius <= map_constants["vikendi"]["5"][1]:
-                    return map_constants["vikendi"]["6"][0]
+                    return map_constants["vikendi"]["6"][0], 5
                 case _ if map_constants["vikendi"]["6"][0] <= radius <= map_constants["vikendi"]["6"][1]:
-                    return map_constants["vikendi"]["7"][0]
+                    return map_constants["vikendi"]["7"][0], 6
                 case _ if map_constants["vikendi"]["7"][0] <= radius <= map_constants["vikendi"]["7"][1]:
-                    return map_constants["vikendi"]["8"][0]
+                    return map_constants["vikendi"]["8"][0], 7
                 case _ if map_constants["vikendi"]["8"][0] <= radius <= map_constants["vikendi"]["8"][1]:
-                    return 0
-    return 0
+                    return 0, 8
+    return 0, 0
+
+def getMapType(center):
+    if center[0] <= 0 or center[1] <= 0:
+        return "None"
+    if center[0] < 1280 and center[1] < 1440:
+        return "e"
+    if center[0] > 1280 and center[1] < 1440:
+        return "v"
+    if center[0] < 1280 and center[1] > 1440:
+        return "s"
+    if center[0] > 1280 and center[1] > 1440:
+        return "m"
+    return "Unknown"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import cv2
 import requests
 
-image_path = "/Users/aadarshsinha/Desktop/VsCode/serverbgmi/TestData/test2.jpg"
+file = "test3.jpg"
+image_path = f"/Users/aadarshsinha/Desktop/VsCode/serverbgmi/TestData/{file}"
 
-url = "http://127.0.0.1:5001/predict"
+url = "http://127.0.0.1:5000/predict"
 files = {"file": open(image_path, "rb")}
 response = requests.post(url, files=files)
 data = response.json()
@@ -15,7 +16,7 @@ radius = int(data["radius"])  # Convert radius to integer
 
 cv2.circle(image, center, radius, (0, 0, 255), 3)
 
-output_path = "/Users/aadarshsinha/Desktop/VsCode/serverbgmi/TestResult/test2.jpg"
+output_path = f"/Users/aadarshsinha/Desktop/VsCode/serverbgmi/TestResult/{file}"
 cv2.imwrite(output_path, image)
 
 print("Circle marked and saved successfully at:", output_path)
